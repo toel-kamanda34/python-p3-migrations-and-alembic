ec29b950c3401110d02b017b8e49996c40dd79dc
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-19 17:40:52 +0300

    completed migrations code along

---
+++ migrations/versions/5fec96f479e4_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 5fec96f479e4
+Revises: 
+Create Date: 2024-09-19 17:04:11.135126
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5fec96f479e4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
